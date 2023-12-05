d8aa64e0c459faaa0a9cf7a0751b34bb5e2c87b0
lucaspaulosx <lp.xavier1@gmail.com>
2023-12-05 18:41:48 -0300

    feat:create page adding components/icons

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'uber': "url('/src/assets/bguber.png')",
+        'city': "url('/src/assets/bg-city.png')",
+      }
+    },
+  },
+  plugins: [],
+}
